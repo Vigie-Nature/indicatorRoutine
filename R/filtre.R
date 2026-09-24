2ea73cea6a0c3ee15606abed333f23708f5fe8c7
Ferdi <ferdinandpetit92@gmail.com>
2026-09-24 17:03:10 +0200

    features/fix ajout de filtres spécifiques par observatoire et correction des makeAnnualsummaryTable.R

---
+++ R/filtre.R
@@ -0,0 +1,68 @@
+
+#' filterData si a function to aplly filter on data based on the methodologies
+#' of the differents observatories
+#'
+#' @param data a `data.frame` containing observations 
+#' @param speciesList a `list` object containing the list of species in the study
+#' @param filters a `character` either "STOC" or "VigieFlore" indicating the methodologies
+#'
+#' @returns
+#' 
+#'
+
+filterData <- function(data, speciesList, filters) {
+
+  # If no filters just making sure speciesList exist
+  if(is.null(filters) && is.null(speciesList)){
+    speciesList <- sort(unique(data$species))
+  }
+
+  # STOC filter
+  # On garde uniquement les observations où la première variable de interestVar est positive.
+  # On regroupe les données par espèce pour calculer
+  # nbOcc = length(unique(ID)) le nombre d’ID uniques par espèce
+  # nbYear = 5 * length(min(year):max(year)) Calcule une valeur seuil pour filtrer les espèces
+  if(filters == "STOC"){
+
+    dataFiltered <- data[data[, interestVar[1]] > 0, ]
+
+    if(!is.null(speciesList)){
+      dataFiltered <- dataFiltered[dataFiltered$species %in% speciesList, ]
+    }
+
+    grData <- dplyr::group_by(dataFiltered, species) %>%
+      dplyr::summarise(nbOcc = length(unique(ID)),
+                      nbYear = 5*length(min(year):max(year)))
+      
+      speciesList <- grData$species[grData$nbOcc > grData$nbYear]
+
+  }
+
+  # Filters for Vigie-Flore
+  # Each species must respect this 2 conditions : 
+  # - At lest 4 years of disticts observations
+  # - At least 10 distinct observations
+  if(filters == "VigieFlore"){
+    dataFiltered <- data[data[, interestVar[1]] > 0, ]
+
+    if(!is.null(speciesList)){
+      dataFiltered <- dataFiltered[dataFiltered$species %in% speciesList, ]
+    }
+
+    grData <- dplyr::group_by(dataFiltered, species) %>%
+      dplyr::summarise(
+        nbSites = length(unique(ID)),
+        nbYear = length(unique(year)),
+        .groups = "drop"
+      )
+
+    speciesList <- grData$species[
+      grData$nbSites >= 10 & grData$nbYear >= 4
+    ]
+
+  }
+
+  if(!is.null(speciesList)){
+    return(speciesList)
+  }
+}
--- R/makeAnnualSummaryTable.R
+++ R/makeAnnualSummaryTable.R
@@ -10,81 +10,69 @@
 #' 
 # Make annual summary table
 makeAnnualSummaryTable <- function(data, sp, interestVar){
-  
-if(length(interestVar) == 1){
-  
+
   # Filter for considered species
-  dataPres_Sp = data[data$species == sp & data[,interestVar[1]]>0,]
+  dataPres_Sp <- data %>% dplyr::filter(
+    species == sp,
+    !!dplyr::sym(interestVar[[1]]) > 0
+  )
+
+  # Define spatial structure ----
+  spatialVars <- if ("point" %in% names(data)) {
+    c("site", "point")
+  } else {
+    c("site")
+  } 
   
-  if(!("point" %in% colnames(data))){
-    
-    # Extract nb of sites visited each year
-    sites <- data %>% 
-      dplyr::group_by(year) %>%
-      dplyr::summarise(nbSites = length(unique(site)))
-    
-    # sum occurences and abundances
-    sumAnnualOcc <- dataPres_Sp %>% 
-      dplyr::group_by(year) %>%
-      dplyr::summarise(Occurrences = length(unique(site)),
-                       Abondance = sum(!!dplyr::sym(interestVar))) %>%
-      dplyr::left_join(sites, by = "year") 
-    
-    # Change colnames
-    colnames(sumAnnualOcc) <- c("Année", "Nb carrés occupés", "Total ind. contactés","Total carrés")
-    
-  }else{
-    
-    # Extract points that have been visited each year
-    sites <- data %>% 
-      dplyr::group_by(year) %>%
-      dplyr::summarise(nbSites = length(unique(paste0(site,point))))
-    
-    sumAnnualOcc <- dataPres_Sp %>% 
-      dplyr::group_by(year) %>%
-      dplyr::summarise(Occurrences = length(unique(paste0(site,point))),
-                       Abondance = sum(!!dplyr::sym(interestVar))) %>%
-      dplyr::left_join(sites, by = "year") 
-    
-    # Change colnames
-    colnames(sumAnnualOcc) <- c("Année", "Nb points occupés", "Total ind. contactés","Total points")
-    
-  }
-  
-  
-}else{
-  
-  if(!("point" %in% colnames(data))){
-    
-  # Extract points that have been visited each year
-  sites <- data %>% 
+  # Extract the number of site/point visited each year
+  data <- data %>%
+    dplyr::mutate(site_id = do.call(paste0, c(dplyr::across(dplyr::all_of(spatialVars)), sep = "_")))
+
+  sites <- data %>%
     dplyr::group_by(year) %>%
-    dplyr::summarise(nbSites = length(unique(site)))
-  
-  # sum occurences
-  sumAnnualOcc <- dataPres_Sp %>% 
-    dplyr::group_by(year) %>%
-    dplyr::summarise(Occurrences = length(unique(site))) %>%
-    dplyr::left_join(sites, by = "year") 
-  
-  }else{
-    
-    # Extract sites that have been visited each year
-    sites <- data %>% 
-      dplyr::group_by(year) %>%
-      dplyr::summarise(nbSites = length(unique(paste0(site,point))))
-    
-    # sum occurences
-    sumAnnualOcc <- dataPres_Sp %>% 
-      dplyr::group_by(year) %>%
-      dplyr::summarise(Occurrences = length(unique(paste0(site,point)))) %>%
-      dplyr::left_join(sites, by = "year")
-    
+    dplyr::summarise(nbSites = dplyr::n_distinct(site_id), .groups = "drop")
+
+  if("point" %in% colnames(data)){
+    if(length(interestVar) == 1) {
+      sumAnnualOcc <- dataPres_Sp %>% 
+        dplyr::group_by(year) %>%
+        dplyr::summarise(Occurrences = length(unique(paste0(site,point))),
+                        Abondance = sum(!!dplyr::sym(interestVar))) %>%
+        dplyr::left_join(sites, by = "year") 
+      
+      # Change colnames
+      colnames(sumAnnualOcc) <- c("Année", "Nb points occupés", "Total ind. contactés","Total points")      
+    } else {
+      # sum occurences
+      sumAnnualOcc <- dataPres_Sp %>% 
+        dplyr::group_by(year) %>%
+        dplyr::summarise(Occurrences = length(unique(paste0(site,point)))) %>%
+        dplyr::left_join(sites, by = "year")
+
+      colnames(sumAnnualOcc) <- c("Année", "Nb de points occupés", "Total de points")
+    }
+  } else {
+    if(length(interestVar) == 1){
+      # sum occurences and abundances
+      sumAnnualOcc <- dataPres_Sp %>% 
+        dplyr::group_by(year) %>%
+        dplyr::summarise(Occurrences = length(unique(site)),
+                        Abondance = sum(!!dplyr::sym(interestVar))) %>%
+        dplyr::left_join(sites, by = "year") 
+      
+      # Change colnames
+      colnames(sumAnnualOcc) <- c("Année", "Nb carrés occupés", "Total ind. contactés","Total carrés")
+    } else {
+      # sum occurences
+      sumAnnualOcc <- dataPres_Sp %>% 
+        dplyr::group_by(year) %>%
+        dplyr::summarise(Occurrences = length(unique(site))) %>%
+        dplyr::left_join(sites, by = "year") 
+
+      # Change colnames
+      colnames(sumAnnualOcc) <- c("Année", "Nb carrés occupés", "Total carrés")
+    }
   }
-  
-  # Change colnames
-  colnames(sumAnnualOcc) <- c("Année", "Nb carrés occupés", "Total ind. contactés","Total carrés")
-}
   
   return(sumAnnualOcc) 
   
